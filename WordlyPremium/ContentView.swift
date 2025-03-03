d7793cb99e3fecef35217083dafcbd632fefab56
Vlad Gotovchykov <vlad.gotovchykov@gmail.com>
2025-03-03 11:10:20 +0100

    Initial Commit

---
+++ WordlyPremium/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WordlyPremium
+//
+//  Created by Vlad Gotovchykov on 03/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WordlyPremium/WordlyPremiumApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordlyPremiumApp.swift
+//  WordlyPremium
+//
+//  Created by Vlad Gotovchykov on 03/03/25.
+//
+
+import SwiftUI
+
+@main
+struct WordlyPremiumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
