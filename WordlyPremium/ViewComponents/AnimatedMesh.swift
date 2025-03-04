8ef24ee127a0b9525fe82f8a2b438efc2ed43353
Ulugbek Abdimurodov <u.abdimurodov@studenti.unisa.it>
2025-03-04 12:00:14 +0100

    animationButtons

---
+++ WordlyPremium/ViewComponents/AnimatedMesh.swift
@@ -0,0 +1,18 @@
+//
+//  AnimatedMesh.swift
+//  WordlyPremium
+//
+//  Created by Ulugbek Abdimurodov on 04/03/25.
+//
+
+import SwiftUI
+
+struct AnimatedMesh: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnimatedMesh()
+}
