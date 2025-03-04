345afeb60c134234b32f250715d355493b6c5150
Vlad Gotovchykov <vlad.gotovchykov@gmail.com>
2025-03-04 10:18:47 +0100

    Added search bar componnent and reusable app colors

---
+++ WordlyPremium/Fonts/Colors.swift
@@ -0,0 +1,22 @@
+//
+//  Colors.swift
+//  WordlyPremium
+//
+//  Created by Vlad Gotovchykov on 03/03/25.
+//
+
+import Foundation
+import SwiftUICore
+
+
+struct AppColors {
+    static let white = Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1))
+    static let polar = Color(#colorLiteral(red: 0.968627451, green: 0.968627451, blue: 0.968627451, alpha: 1))
+    static let gray: Color = Color(#colorLiteral(red: 0.6862745098, green: 0.6862745098, blue: 0.6862745098, alpha: 1))
+    static let eel: Color = Color(#colorLiteral(red: 0.2941176471, green: 0.2941176471, blue: 0.2941176471, alpha: 1))
+    static let ligthGreen: Color = Color(#colorLiteral(red: 0.3450980392, green: 0.8, blue: 0.007843137255, alpha: 1))
+    static let darkGreen: Color = Color(#colorLiteral(red: 0.3450980392, green: 0.6549019608, blue: 0, alpha: 1))
+    static let lightBlue: Color = Color(#colorLiteral(red: 0.1098039216, green: 0.6901960784, blue: 0.9647058824, alpha: 1))
+    static let gradient: Gradient = .init(colors: [Color(#colorLiteral(red: 0.5647058824, green: 0.4117647059, blue: 0.8039215686, alpha: 1)), Color(#colorLiteral(red: 1, green: 0.6980392157, blue: 0.6980392157, alpha: 1)),Color(#colorLiteral(red: 1, green: 0.7843137255, blue: 0, alpha: 1)),Color(#colorLiteral(red: 0.3450980392, green: 0.8, blue: 0.007843137255, alpha: 1))])
+
+}
--- WordlyPremium/ViewComponents/SearchBar.swift
+++ WordlyPremium/ViewComponents/SearchBar.swift
@@ -8,8 +8,41 @@
 import SwiftUI
 
 struct SearchBar: View {
+    
+    @State var searchText: String = ""
+    
+    var guidingText: String = "Search your pack..."
+    let imageName: String = "magnifier"
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Image(imageName)
+                .padding(4)
+            
+            TextField(guidingText, text: $searchText)
+                .font(.custom("feather", size: 16))
+                .padding(.trailing, 30)
+
+                .overlay (
+                    Image("cross")
+                        .opacity(searchText.isEmpty ? 0.0 : 1.1)
+                        .offset(x: 5)
+                        .padding(5)
+                        .animation(.easeIn(duration: 0.2), value: searchText.isEmpty)
+                        .onTapGesture {
+                            searchText = ""
+                        }
+                    ,alignment: .trailing
+                )
+        }
+        .padding(5)
+        .background {
+            RoundedRectangle(cornerRadius: 2)
+                .fill(AppColors.white)
+                .stroke(AppColors.gray, style: .init(lineWidth: 1))
+        }
+       
     }
 }
 
