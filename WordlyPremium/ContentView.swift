bb0a1ae0ca3594a096e21a3bf6fa836444a557ed
Jahongir Abdujalilov <jabdujalilov24@fed.idserve.net>
2025-03-07 14:36:52 +0100

    Remove Alamofire dependency and related files; update Info.plist for App Transport Security settings

--- WordlyPremium/ContentView.swift
+++ WordlyPremium/ContentView.swift
@@ -81,3 +81,50 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
+//@StateObject private var viewModel = PackSliderViewModel()
+//@State private var showAddView = false
+//
+//var body: some View {
+//    NavigationStack {
+//        VStack {
+//            HStack {
+//                AddButton()
+//                    .padding()
+//                    .padding(.vertical, -30)
+//                    .padding(.top, 20)
+//                    .onTapGesture {
+//                        showAddView = true
+//                    }
+//                    .sheet(isPresented: $showAddView) {
+//                        AddPackView()
+//                            .presentationDetents([.fraction(0.5)])
+//                            .presentationDragIndicator(.visible)
+//                    }
+//            }
+//            .frame(maxWidth: .infinity, alignment: .trailing)
+//            SearchBar()
+//                .padding()
+//        }
+//        ScrollView {
+//            VStack(spacing: 35) {
+//                CardSlider(
+//                    packTitle: "Your Packs",
+//                    packNumber: viewModel.packs.count,
+//                    cards: viewModel.packs
+//                )
+//                CardSlider(
+//                    packTitle: "Your Folders",
+//                    packNumber: viewModel.folders.count,
+//                    cards: viewModel.folders
+//                )
+//                CardSlider(
+//                    packTitle: "Community Packs",
+//                    packNumber: viewModel.community.count,
+//                    cards: viewModel.community
+//                )
+//            }
+//            .padding(.top, 20)
+//        }
+//    }
+//}
--- WordlyPremium/Models/Models.swift
+++ WordlyPremium/Models/Models.swift
@@ -7,6 +7,29 @@
 
 import Foundation
 
+// Models for Components
+// Folder
+struct Folder: Codable, Identifiable {
+    let id: UUID = UUID()
+    let name: String
+    var packs: [Pack]
+    
+    enum CodingKeys: String, CodingKey {
+        case name, packs
+    }
+}
+
+// Pack
+struct Pack: Codable, Identifiable {
+    let id: UUID = UUID()
+    var name: String
+    let isAIGenerated: Bool
+    var flashcards: [Flashcard]
+    
+    enum CodingKeys: String, CodingKey {
+        case name, isAIGenerated, flashcards
+    }
+}
 
 // Request Model
 struct GenerateCardsRequest: Codable {
@@ -32,8 +55,8 @@ struct FlashcardResponse: Codable {
 // Flashcard Model
 struct Flashcard: Codable, Identifiable {
     let id: UUID = UUID()
-    let question: String
-    let answer: String
+    var question: String
+    var answer: String
 
     enum CodingKeys: String, CodingKey {
         case question = "langFrom"
